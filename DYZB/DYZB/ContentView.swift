2776f04c26fa0c660cdc154292db55fd49675a7f
zhangliangliang <zhangliangliang@le.com>
2019-06-12 17:07:44 +0800

    first commit

---
+++ DYZB/DYZB/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DYZB
+//
+//  Created by leeco on 2019/6/12.
+//  Copyright © 2019 leeco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
